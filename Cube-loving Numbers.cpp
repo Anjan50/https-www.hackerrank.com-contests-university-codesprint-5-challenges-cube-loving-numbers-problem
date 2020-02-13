db25a664afbf49ff06de1d77129c25f81167d001
Anjan Diyora <49095535+Anjan50@users.noreply.github.com>
2020-02-13 23:44:40 +0530

    Update and rename README.md to Cube-loving Numbers.cpp

---
+++ Cube-loving Numbers.cpp
@@ -0,0 +1,61 @@
+# https-www.hackerrank.com-contests-university-codesprint-5-challenges-cube-loving-numbers-problem
+Cube-loving Numbers
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+// Complete the solve function below.
+long solve(long n) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        long n = stol(ltrim(rtrim(n_temp)));
+
+        long result = solve(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# https-www.hackerrank.com-contests-university-codesprint-5-challenges-cube-loving-numbers-problem
-Cube-loving Numbers
